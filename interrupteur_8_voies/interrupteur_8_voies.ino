aec9c7f0559abe21edaf947229000745be456e61
Noah0070 <80363186+Noah0070@users.noreply.github.com>
2023-06-09 10:25:54 +0200

    Création du fichier

---
+++ interrupteur_8_voies/interrupteur_8_voies.ino
@@ -0,0 +1,19 @@
+
+/*
+ * 1 => BROCHE 3
+ * 2 => BROCHE 4
+ * 3 => BROCHE 5
+ * 4 => BROCHE 6 
+ * 5 => BROCHE A0
+ * 6 => BROCHE A3
+ * 7 => BROCHE A4
+ * 8 => BROCHE A5
+ */
+
+void setup() {
+  pinMode(A0, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(A0, HIGH);
+}
