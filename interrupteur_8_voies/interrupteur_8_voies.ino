68c4ba38c3a018b9b87b0150c3c9c64e1ffb658f
Noah0070 <80363186+Noah0070@users.noreply.github.com>
2023-06-09 11:22:45 +0200

    Récupérer une valeur à partir des interrupteurs

--- interrupteur_8_voies/interrupteur_8_voies.ino
+++ interrupteur_8_voies/interrupteur_8_voies.ino
@@ -1,4 +1,3 @@
-
 /*
  * 1 => BROCHE 3
  * 2 => BROCHE 4
@@ -10,10 +9,64 @@
  * 8 => BROCHE A5
  */
 
+
+ int definirHeure(){
+  /* 
+   *  Chaque bouton poussoir est associé à un chiffre.
+   *  Pour fixer une heure, il faut activer des boutons poussoir de tels sorte 
+   *  que la somme des chiffres qui leurs sont associés soit égale à l'heure 
+   *  que l'ont désire fixer.
+   */
+  int heure = 0;
+
+  if (digitalRead(3) > 0) { //Bouton 1
+     heure ++;
+  }
+  if (digitalRead(4) > 0) { // Bouton 2
+    heure += 2;
+  }
+  if (digitalRead(5) > 0) { // Bouton 3
+    heure += 3;
+  }
+  if (digitalRead(6) > 0) { // Bouton 4
+    heure += 4;
+  }
+  if (digitalRead(A0) > 0) { // Bouton 5
+    heure += 5;
+  }
+  if (digitalRead(A3) > 0) { // Bouton 6
+    heure += 6;
+  }
+  if (digitalRead(A4) > 0) { // Bouton 7
+    heure += 7;
+  }
+  if (digitalRead(A5) > 0) { // Bouton 8
+    heure += 8;
+  }
+
+  return heure;
+ }
+
 void setup() {
-  pinMode(A0, OUTPUT);
+  pinMode(2, INPUT); // Bouton
+  pinMode(A0, INPUT);
+  pinMode(A3, INPUT);  
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(A0, HIGH);
+  if (digitalRead(2) == 1) { // Si le bouton est pressé...
+    Serial.println("Bouton pressé...");
+    while(digitalRead(2) == 1); // On attend que le bouton repasse à l'état bas une fois car avant les données sont souvent erronées
+    Serial.println(definirHeure());
+    Serial.println("ATTENTION : attendez 5 secondes avant de presser le bouton à nouveau !"); // Car le bouton ne pas pas à l'état bas directement après le relachement
+  }
+  
+  delay(200);
 }
